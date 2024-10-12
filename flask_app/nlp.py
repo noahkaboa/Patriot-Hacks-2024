558893f53b9f776abe39fb7949ede278fe10a407
noahkaboa <nphinger@gmail.com>
2024-10-12 19:31:32 -0400

    Recommendations

--- flask_app/nlp.py
+++ flask_app/nlp.py
@@ -2,7 +2,7 @@ import spacy # pip install spacy
 import json # built-in
 
 # python3 -m spacy download en_core_web_lg
-nlp = spacy.load("en_core_web_lg")
+nat_lang_proc = spacy.load("en_core_web_lg")
 
 
 def rank_businesses(preferences, num_results=5):
@@ -12,21 +12,26 @@ def rank_businesses(preferences, num_results=5):
     scores = {}
 
     for business_name, business_data in directory.items():
+        
         description = business_data["description"]
         
-        bus_doc = nlp(business_name + ": " + description)
+        bus_doc = nat_lang_proc(business_name + ": " + description)
+
 
         score = 0
 
-        for preference in preferences:
-            pref_doc = nlp(preference)
-            score += bus_doc.similarity(pref_doc)
-        
+        try:
+            for preference in preferences:
+                pref_doc = nat_lang_proc(preference)
+                score += bus_doc.similarity(pref_doc)
+        except Exception as e:
+            print(f"Error: {e}")
+
         scores[business_name] = score
-    
+
     sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-    return sorted_scores[:num_results]
+    return sorted_scores[:num_results].items()
 
 
 
-# print(rank_businesses(["cafe", "homemade", "coffee", "3d printing", "lunch", "wifi", "vegan"], 5))
+# print(rank_businesses(["coffee", "chess"], 5))
